1e033b62a28d4873c91467e98f323c01945b9453
GabrielPortes25 <gabrielportescac@gmail.com>
2025-09-23 13:46:05 -0300

    Projeto Finalizado

---
+++ src/DadosCliente.java
@@ -0,0 +1,91 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class DadosCliente {
+    public static void main(String[] args) {
+
+        Scanner leitura = new Scanner(System.in);
+        int randomNumber = new Random().nextInt(4000);
+        String tipoConta = "";
+        int numeroConta = 0;
+
+        System.out.println("--------------------------------------------------------");
+        System.out.println("Digite o nome do cliente: ");
+        String nomeCliente = leitura.next();
+        System.out.println("--------------------------------------------------------");
+        System.out.println("ditite o tipo de conta do " + nomeCliente);
+        System.out.println("Digite 1 para conta corrente!");
+        System.out.println("--------------------------------------------------------");
+         numeroConta = leitura.nextInt();
+
+
+
+        while(numeroConta != 1){
+            System.out.println("--------------------------------------------------------");
+            System.out.println("-----DIGITE UMA OPÇÃO VALIDA!!!-----");
+            System.out.println("ditite o tipo de conta do " + nomeCliente);
+            System.out.println("Digite 1 para conta corrente!");
+            numeroConta = leitura.nextInt();
+        }
+
+        if (numeroConta == 1){
+            tipoConta = "Corrente";
+        }
+        System.out.println("*************************************************");
+        System.out.println("Dados iniciais do cliente:");
+        System.out.println("Nome: " + nomeCliente);
+        System.out.println("Tipo de conta: " + tipoConta);
+        System.out.println("Saldo Inicial: R$ " + randomNumber);
+        System.out.println("*************************************************");
+
+        System.out.println("Operações:");
+        System.out.println("1- Consultar valor");
+        System.out.println("2- Receber valor");
+        System.out.println("3- Transferir valor");
+        System.out.println("4- sair");
+        System.out.println("Digite a operação desejada:");
+        int operacoes = leitura.nextInt();
+
+
+        while (operacoes != 4){
+
+            if(operacoes == 1){
+                System.out.println("--------------------------------------------------");
+                System.out.println("Seu saldo é: R$ " + randomNumber);
+                System.out.println("--------------------------------------------------");
+            }else if(operacoes == 2){
+                System.out.println("--------------------------------------------------");
+                System.out.println("Digite o valor que você deseja depositar: ");
+                int novoSaldo = leitura.nextInt();
+                randomNumber += novoSaldo;
+                System.out.println("Seu novo saldo é de: R$ " + randomNumber);
+                System.out.println("--------------------------------------------------");
+
+            }else if (operacoes == 3) {
+                System.out.println("--------------------------------------------------");
+                System.out.println("Digite o valor que você deseja transferir: ");
+                int novoSaldo = leitura.nextInt();
+                randomNumber -= novoSaldo;
+                System.out.println("Seu novo saldo é de: R$ " + randomNumber);
+                System.out.println("--------------------------------------------------");
+
+            }else if (operacoes != 4) {
+                System.out.println("--------------------------------------------------");
+                System.out.println("OPERAÇÃO INVALIDA!!!");
+                System.out.println("--------------------------------------------------");
+            }
+            System.out.println("------------------------------------------------------");
+            System.out.println("Operações:");
+            System.out.println("1- Consultar valor");
+            System.out.println("2- Receber valor");
+            System.out.println("3- Transferir valor");
+            System.out.println("4- sair");
+            System.out.println("Digite a operação desejada:");
+            operacoes = leitura.nextInt();
+        }
+
+    }
+}
+
+
+
